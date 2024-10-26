55a2cee4a2a52eb88d9e075ec24e7394f173a9e0
Mykhailo Muzyka <mykhailo.muzyka535@gmail.com>
2024-10-26 21:00:27 +0300

    Add app info

--- src/ui/Application.cpp
+++ src/ui/Application.cpp
@@ -9,6 +9,8 @@ int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
+    app.setOrganizationName("CustomPlayer");
+    app.setApplicationName("customplayer");
 
     QQmlApplicationEngine engine;
 
